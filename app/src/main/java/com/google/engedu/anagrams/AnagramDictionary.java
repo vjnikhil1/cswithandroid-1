f46ea7f950fcf912be3285c7044ed538d461598e
vjnikhil1 <johnnikhil95@gmail.com>
2016-03-25 21:32:22 +0530

    Initial commit

---
+++ app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
@@ -0,0 +1,66 @@
+package com.google.engedu.anagrams;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Random;
+
+public class AnagramDictionary {
+
+    private static final int MIN_NUM_ANAGRAMS = 5;
+    private static final int DEFAULT_WORD_LENGTH = 3;
+    private static final int MAX_WORD_LENGTH = 7;
+    private Random random = new Random();
+    private ArrayList<String> wordList = new ArrayList<>();
+    private HashSet<String> wordSet = new HashSet<>();
+    private HashMap<String, ArrayList<String>> lettersToWord = new HashMap<>();
+
+    public AnagramDictionary(InputStream wordListStream) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(wordListStream));
+        String line;
+        while((line = in.readLine()) != null) {
+            String word = line.trim();
+            wordList.add(word);
+            wordSet.add(word);
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String newWord = new String(chars);
+            if(lettersToWord.get(newWord)!=null)
+                lettersToWord.get(newWord).add(word);
+            else{
+                ArrayList<String> values = new ArrayList<>();
+                values.add(word);
+                lettersToWord.put(newWord, values);
+            }
+        }
+    }
+
+    public boolean isGoodWord(String word, String base) {
+        if(wordSet.contains(word)&&!word.contains(base))
+            return true;
+        else
+            return false;
+    }
+
+    public ArrayList<String> getAnagramsWithOneMoreLetter(String word) {
+        ArrayList<String> result = new ArrayList<>();
+        for(int i='a';i<='z';i++){
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String nw = new String(chars);
+            if (lettersToWord.get(nw) != null) {
+                lettersToWord.get(nw).addAll(result);
+            }
+        }
+        return result;
+    }
+
+    public String pickGoodStarterWord() {
+        
+    }
+}
